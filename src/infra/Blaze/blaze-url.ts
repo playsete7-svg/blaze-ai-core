94ce4600cd0f6bacaba64a5bdb2f44eb0121e169
playsete7-svg <playsete7@gmail.com>
2026-06-03 10:50:33 -0300

    Delete src/infra/Blaze directory

--- src/infra/Blaze/blaze-url.ts
+++
@@ -1,8 +0,0 @@
-export const getBlazeUrl = (type: 'games' | 'general'): string => {
-  switch (type) {
-    case 'games':
-      return 'wss://api-gaming.blaze.bet.br/replication/?EIO=3&transport=websocket'
-    case 'general':
-      return 'wss://api-v2.blaze.bet.br/replication/?EIO=3&transport=websocket'
-  }
-}
